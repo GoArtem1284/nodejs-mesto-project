15bef2b3ba6b4ea48f28f566ba61b65f65b61ba3
Artem Gordienko <morrag1986@gmail.com>
2026-08-27 11:02:39 +0100

    cards validation

---
+++ src/validators/cards.ts
@@ -0,0 +1,17 @@
+import { celebrate, Joi } from 'celebrate';
+import { URL_REGEX } from '../utils/urlRegex';
+
+const objectId = Joi.string().hex().length(24);
+
+export const validateCreateCard = celebrate({
+  body: Joi.object({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().pattern(URL_REGEX),
+  }),
+});
+
+export const validateCardId = celebrate({
+  params: Joi.object({
+    cardId: objectId.required(),
+  }),
+});
